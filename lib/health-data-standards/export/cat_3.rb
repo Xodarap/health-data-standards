4e2990f4340a3167a460b6435a4247ddd0ae14a9
xodarap <xodarap00@gmail.com>
2014-12-30 14:05:31 -0800

    IG updates

--- lib/health-data-standards/export/cat_3.rb
+++ lib/health-data-standards/export/cat_3.rb
@@ -7,6 +7,14 @@ module HealthDataStandards
         @rendering_context.template_helper = template_helper
         @cat1_renderer = HealthDataStandards::Export::RenderingContext.new
         @cat1_renderer.template_helper = HealthDataStandards::Export::TemplateHelper.new('cat1', 'cat1')
+        @supplemental_info = get_supplemental_info
+      end
+
+      def get_supplemental_info
+        vset = HealthDataStandards::SVS::ValueSet
+        {:race => vset.where({:oid => '2.16.840.1.114222.4.11.836'}).first.concepts,
+         :sex => ['F','M','UN'],  # 2.16.840.1.113883.1.11.1
+         :ethnicity => vset.where({:oid => '2.16.840.1.114222.4.11.837'}).first.concepts}
       end
 
       def export(measures, header, effective_date, start_date, end_date, filter=nil,test_id=nil)
@@ -17,7 +25,7 @@ module HealthDataStandards
         @rendering_context.render(:template => 'show', 
                                   :locals => {:measures => measures, :start_date => start_date, 
                                               :end_date => end_date, :cat1_renderer => @cat1_renderer,
-                                              :results => results,
+                                              :results => results, :supplemental_info => @supplemental_info,
                                               :header=>header})
       end
     end
